717d7a12409dbe2a860d4b3d981239d4ac9fb425
Marlon Yepes <myepes82@misena.edu.co>
2023-12-14 23:21:37 -0500

    Library code updated

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const LOGGING_LEVEL_COLORS = {
-	WARN: "\u001B[33m",
-	ERROR: "\u001B[31m",
-	DEBUG: "\u001B[32m",
-	INFO: "\u001B[90m",
+const LOGGING_LEVEL_COLORS: Readonly<Record<string, string>> = {
+	"WARN": "\u001B[33m",
+	"ERROR": "\u001B[31m",
+	"DEBUG": "\u001B[32m",
+	"INFO": "\u001B[90m",
 }
 
-function getCurrentFormattedDate() {
+function getCurrentFormattedDate(): string {
 	const options = {
 		year: "numeric",
 		month: "numeric",
@@ -13,16 +13,15 @@ function getCurrentFormattedDate() {
 		hour: "numeric",
 		minute: "numeric",
 		timeZone: "UTC",
-	}
+	}  satisfies Intl.DateTimeFormatOptions
 	return new Date().toLocaleString("en-US", options)
 }
 
-function underLineText(text) {
+function underLineText(text: string): string {
 	return `\u001b[4m${text}\u001b[0m`
 }
-
-function messageParser(loggingLevel, message, ...args) {
-	const loggingColor = LOGGING_LEVEL_COLORS[loggingLevel]
+function messageParser(loggingLevel: string, message: string, ...args: any[]): string {
+	const loggingColor: string = LOGGING_LEVEL_COLORS[loggingLevel]
 	const formattedDate = `(${underLineText(getCurrentFormattedDate())})`
 	let parsedMessage = message
 	if (args.length > 0) {
@@ -30,19 +29,18 @@ function messageParser(loggingLevel, message, ...args) {
 	}
 	return `${loggingColor}[${loggingLevel}]\x1b[0m: ${formattedDate} ${loggingColor}=>\x1b[0m ${parsedMessage}`
 }
-
-export function logWarn(message, ...args) {
+export function logWarn(message: string, ...args: string[]): void {
 	console.warn(messageParser("WARN", message, args))
 }
 
-export function logError(message, ...args) {
+export function logError(message: string, ...args: string[]): void {
 	console.error(messageParser("ERROR", message, args))
 }
 
-export function logDebug(message, ...args) {
+export function logDebug(message: string, ...args: string[]): void {
 	console.log(messageParser("DEBUG", message, args))
 }
 
-export function logInfo(message, ...args) {
+export function logInfo(message: string, ...args: string[]): void {
 	console.info(messageParser("INFO", message, args))
 }
